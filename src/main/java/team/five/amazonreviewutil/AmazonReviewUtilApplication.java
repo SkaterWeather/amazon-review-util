7dd82726e78dcf240b9b57ba02956a54df7c9957
Max <skaterweather41@gmail.com>
2019-11-04 15:29:39 +0200

    initial commit

---
+++ src/main/java/team/five/amazonreviewutil/AmazonReviewUtilApplication.java
@@ -0,0 +1,13 @@
+package team.five.amazonreviewutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/five/amazonreviewutil/AmazonReviewUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package team.five.amazonreviewutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
