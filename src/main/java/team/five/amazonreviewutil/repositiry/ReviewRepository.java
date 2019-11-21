c35420241c56f8bce9f3581d80b8a04869a75481
Max <skaterweather41@gmail.com>
2019-11-22 00:25:07 +0200

    few CS improvements

--- src/main/java/team/five/amazonreviewutil/repositiry/ReviewRepository.java
+++ src/main/java/team/five/amazonreviewutil/repositiry/ReviewRepository.java
@@ -11,18 +11,17 @@ import team.five.amazonreviewutil.entity.Review;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query(value =
-            "SELECT profile_name FROM reviews GROUP BY profile_name ORDER BY COUNT (profile_name) DESC LIMIT 1000;",
+            "SELECT profile_name FROM reviews GROUP BY profile_name "
+                    + "ORDER BY COUNT (profile_name) DESC LIMIT 1000;",
             nativeQuery = true)
     List<String> getMostActiveUserProfiles();
 
     @Query(value =
-            "SELECT product_id FROM reviews GROUP BY product_id ORDER BY COUNT (product_id) DESC LIMIT 1000;",
+            "SELECT product_id FROM reviews GROUP BY product_id "
+                    + "ORDER BY COUNT (product_id) DESC LIMIT 1000;",
             nativeQuery = true)
     List<String> getMostCommentedItemsId();
 
-    //todo: problem - will return emptyList with profile_name's like - "C. F. Hill ""CFH"""
-    //just a test method
-    //comments will be removed before merge
     @Query(value =
             "SELECT * FROM reviews WHERE profile_name=:name ;",
             nativeQuery = true)
