59c47432bf4fbf19a7e9fad3ddbeccbef93aed81
Max <skaterweather41@gmail.com>
2019-11-22 02:20:35 +0200

    implemented controller; added intex.html and Swagger2

--- src/main/java/team/five/amazonreviewutil/AmazonReviewUtilApplication.java
+++ src/main/java/team/five/amazonreviewutil/AmazonReviewUtilApplication.java
@@ -2,8 +2,9 @@ package team.five.amazonreviewutil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "team.five.amazonreviewutil" }, exclude = { ErrorMvcAutoConfiguration.class })
 public class AmazonReviewUtilApplication {
 
     public static void main(String[] args) {
